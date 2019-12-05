62b61ed6b3ca1b86dd324a72d6a35ca01a6b7a49
HubCodes <krlrhkstk12@gmail.com>
2019-12-05 22:21:23 +0900

    Initial implementation

---
+++ getter/Program.fs
@@ -0,0 +1,27 @@
+﻿open System.Net
+open System.IO
+
+let get (url: string) =
+    async {
+        let request = WebRequest.Create url
+        let! response = request.AsyncGetResponse()
+        let responseStream = response.GetResponseStream()
+        use streamReader = new StreamReader(responseStream)
+        let result = streamReader.ReadToEnd()
+
+        return result
+    }
+
+[<EntryPoint>]
+let main argv =
+    Async.Parallel
+        [ for url in argv ->
+            async {
+                let! result = get url
+                printfn "%s" result
+
+                return result
+            } ]
+    |> Async.RunSynchronously
+    |> ignore
+    0
