2e70b1b285e8bcd4dbb81a44a598dc1dd37922e1
HubCodes <krlrhkstk12@gmail.com>
2019-12-08 15:16:14 +0900

    Add exception handling

--- getter/Program.fs
+++ getter/Program.fs
@@ -1,15 +1,20 @@
-﻿open System.Net
+﻿open System
+open System.Net
 open System.IO
 
 let get (url: string) =
     async {
-        let request = WebRequest.Create url
-        let! response = request.AsyncGetResponse()
-        let responseStream = response.GetResponseStream()
-        use streamReader = new StreamReader(responseStream)
-        let result = streamReader.ReadToEnd()
+        try
+            let request = WebRequest.Create url
+            use! response = request.AsyncGetResponse()
+            use responseStream = response.GetResponseStream()
+            use streamReader = new StreamReader(responseStream)
+            let result = streamReader.ReadToEnd()
 
-        return result
+            return Ok result
+        with
+        | :? System.UriFormatException -> return Error "Malformed URL"
+        | :? System.Net.WebException as e -> return Error e.Message
     }
 
 [<EntryPoint>]
@@ -18,7 +23,9 @@ let main argv =
         [ for url in argv ->
             async {
                 let! result = get url
-                printfn "%s" result
+                match result with
+                | Ok data -> printfn "%s" data
+                | Error e -> Console.Error.WriteLine("{0}", e)
 
                 return result
             } ]
